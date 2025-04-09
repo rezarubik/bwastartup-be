bcb41d1970a4b3e2dfdb8a8489b3981e76941b83
Reza Pahlevi <reza.pahlevi@uangme.com>
2025-04-09 09:44:32 +0700

    Campaign repository

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,8 @@
 package handler
+
+// todo: Steps:
+// * tangkap parameter di handler
+// * handler ke service
+// * service yang menentukan repository mana yang di-call
+// * repository: FindAll, FindByUserID
+// * db
